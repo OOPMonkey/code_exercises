22504630f55b17ad4fb50c3b6d237be5d9b9552c
oopmonkey <dddworld@gmail.com>
2014-05-27 11:56:24 -0400

    added description file. Completed hello world and adding comments about tasks

--- c++/daily_programmer/164_assemble_this_scheme_into_python/main.cpp
+++ c++/daily_programmer/164_assemble_this_scheme_into_python/main.cpp
@@ -1,8 +1,22 @@
 #include <QCoreApplication>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    // Output 'Hello World' to the console.
+    std::cout << "Hello World!";
+
+    // Return an array of the first 100 numbers that are divisible by 3 and 5.
+
+    // Create a program that verifies if a word is an anagram of another word.
+
+    // Create a program that removes a specificed letter from a word.
+
+    // Sum all the elements of an array
+
+    // BONUS: Implement a bubble-sort.
 
     return a.exec();
 }
