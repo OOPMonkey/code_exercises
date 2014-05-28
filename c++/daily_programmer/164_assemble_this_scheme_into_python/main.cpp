50fed552f23ad7a5c345764f2e44d1968c408659
oopmonkey <dddworld@gmail.com>
2014-05-27 22:36:20 -0400

    Now removes letters from words. Removed build and backup files

--- c++/daily_programmer/164_assemble_this_scheme_into_python/main.cpp
+++ c++/daily_programmer/164_assemble_this_scheme_into_python/main.cpp
@@ -1,22 +1,39 @@
 #include <QCoreApplication>
 #include <iostream>
+#include <QTextStream>
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    using namespace std;
+
+    QTextStream qin(stdin);
+    QTextStream qout(stdout);
 
     // Output 'Hello World' to the console.
-    std::cout << "Hello World!";
+    qout << "Hello World!" << endl;
 
     // Return an array of the first 100 numbers that are divisible by 3 and 5.
+    for(int i = 0; i <= 100; ++i)
+    {
+        if(i % 3 == 0 && i % 5 == 0)
+            qout << i << endl;
+    }
 
     // Create a program that verifies if a word is an anagram of another word.
 
     // Create a program that removes a specificed letter from a word.
+    qout << "Please input a word:" << endl;
+    QString targetWord;
+    qin >> targetWord;
+
+    qout << endl << "Input a letter to be removed the word you just entered: " << endl;
+    QString targetLetter;
+    qin >> targetLetter;
+    qout << targetWord.replace(targetLetter, "") << endl;
 
     // Sum all the elements of an array
 
     // BONUS: Implement a bubble-sort.
 
-    return a.exec();
+    return 0;
 }
