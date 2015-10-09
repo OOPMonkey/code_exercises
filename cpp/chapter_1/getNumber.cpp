d79d8b1e8b48cdf6a0aaacfc8aaf2835fd241c0e
Dom <flossbeast@gmail.com>
2015-10-08 20:50:10 -0400

    worked on a few chapter 1 examples

---
+++ cpp/chapter_1/getNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+	using std::cout;
+	using std::endl;
+
+	cout << "Enter a number:" << endl;
+
+	int x = 0;
+	std::cin >> x;
+
+	cout << "You entered: " << x << endl;
+	return 0;
+}
